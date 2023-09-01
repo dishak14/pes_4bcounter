bb18917126be4c738e2e48f9aee37e422746c823
dishak14 <92496153+dishak14@users.noreply.github.com>
2023-09-01 14:36:01 +0530

    Update pes_4bcounter.v

--- pes_4bcounter.v
+++ pes_4bcounter.v
@@ -1,36 +1,33 @@
-module tb_counter;
-
-    // Inputs
-    reg Clk;
-    reg reset;
-    reg UpOrDown;
-
-    // Outputs
-    wire [3:0] Count;
-
-    // Instantiate the Unit Under Test (UUT)
-    iiitb_4bbc uut (
-        .Clk(Clk), 
-        .reset(reset), 
-        .UpOrDown(UpOrDown), 
-        .Count(Count)
+module iiitb_4bbc(
+    Clk,
+    reset,
+    UpOrDown,  //high for UP counter and low for Down counter
+    Count
     );
 
-//Generate clock with 10 ns clk period.
-    initial Clk = 0;
-    always #5 Clk = ~Clk;
     
-    initial begin
-        // Apply Inputs
-        reset = 0;
-        UpOrDown = 0;
-        #300;
-        UpOrDown = 1;
-      #300;
-        reset = 1;
-        UpOrDown = 0;
-        #100;
-        reset = 0;  
-    end
-      
+    //input ports and their sizes
+    input Clk,reset,UpOrDown;
+    //output ports and their size
+    output [3 : 0] Count;
+    //Internal variables
+    reg [3 : 0] Count = 0;  
+    
+     always @(posedge(Clk) or posedge(reset))
+     begin
+        if(reset == 1) 
+            Count <= 0;
+        else    
+            if(UpOrDown == 1)   //Up mode selected
+                if(Count == 15)
+                    Count <= 0;
+                else
+                    Count <= Count + 1; //Incremend Counter
+            else  //Down mode selected
+                if(Count == 0)
+                    Count <= 15;
+                else
+                    Count <= Count - 1; //Decrement counter
+     end    
+    
 endmodule
